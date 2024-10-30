e7f80116361fd06c08ae69abdc70535f7fc756a2
Amin <min123kabir@gmail.com>
2024-10-30 15:27:22 +0100

    added content to categories

--- src/components/ShopList/ShopItem.jsx
+++ src/components/ShopList/ShopItem.jsx
@@ -1,7 +1,7 @@
-export default function ShopItem(item, index) {
+function PhoneItem(item, index) {
   return (
     <div key={index}>
-      <div className="bg-cover bg-center center h-[300px] w-52 shadow-xl" style={{backgroundImage: `url("/${item.img}")`}}>
+      <div className="bg-cover bg-center h-[300px] w-52 shadow-xl" style={{backgroundImage: `url("/phones/${item.img}")`}}>
 
       </div>
       <p className="text-2xl mt-5 mb-1">{item.name}</p>
@@ -10,3 +10,44 @@ export default function ShopItem(item, index) {
     </div>
   )
 }
+
+function TVItem(item, index) {
+  return (
+    <div key={index}>
+      <div className="bg-cover bg-center h-[200px] w-60 shadow-xl" style={{backgroundImage: `url("/tvs/${item.img}")`}}>
+
+      </div>
+      <p className="text-2xl mt-5 mb-1">{item.name}</p>
+      <p>${item.price}</p>
+      <button className="bg-foreground py-2 px-12 w-full rounded text-white mt-3 hover:bg-hoverColor">Add to cart</button>
+    </div>
+  )
+}
+
+function LaptopItem(item, index) {
+  return (
+    <div key={index}>
+      <div className="bg-cover bg-center h-[200px] w-60 shadow-xl" style={{backgroundImage: `url("/laptops/${item.img}")`}}>
+
+      </div>
+      <p className="text-2xl mt-5 mb-1">{item.name}</p>
+      <p>${item.price}</p>
+      <button className="bg-foreground py-2 px-12 w-full rounded text-white mt-3 hover:bg-hoverColor">Add to cart</button>
+    </div>
+  )
+}
+
+function SmartwatchItem(item, index) {
+  return (
+    <div key={index}>
+      <div className="bg-cover bg-center h-[200px] w-60 shadow-xl" style={{backgroundImage: `url("/smartwatches/${item.img}")`}}>
+
+      </div>
+      <p className="text-2xl mt-5 mb-1">{item.name}</p>
+      <p>${item.price}</p>
+      <button className="bg-foreground py-2 px-12 w-full rounded text-white mt-3 hover:bg-hoverColor">Add to cart</button>
+    </div>
+  )
+}
+
+export { PhoneItem, TVItem, LaptopItem, SmartwatchItem }
--- src/components/ShopList/ShopList.jsx
+++ src/components/ShopList/ShopList.jsx
@@ -1,43 +1,16 @@
-import ShopItem from "./ShopItem"
 
-const devices = [
-  {
-    name: "iPhone 16",
-    img: "ip16.jpg",
-    price: "699"
-  },
-  {
-    name: "iPhone 16 Pro",
-    img: "ip16p.webp",
-    price: "649"
-  },
-  {
-    name: "iPhone 16 Pro Max",
-    img: "ip16pm.jpg",
-    price: "799"
-  },
-  {
-    name: "iPhone 15",
-    img: "ip15.webp",
-    price: "599"
-  },
-  {
-    name: "iPhone 14",
-    img: "ip14.webp",
-    price: "499.99"
-  },
-  {
-    name: "Samsung S22",
-    img: "s22.webp",
-    price: "699"
-  }
-]
+import { PhoneItem, TVItem, LaptopItem, SmartwatchItem } from "./ShopItem"
+import { phones, tvs, laptops, smartwatches } from "./data"
 
 export default function ShopList(props) {
 
+
   return (
     <div className="flex gap-20 flex-wrap">
-      {props.category == "phones" ? devices.map(ShopItem) : <h1>not Found</h1>}
+      {props.category == "phones" && phones.map(PhoneItem)}
+      {props.category == "televisions" && tvs.map(TVItem)}
+      {props.category == "laptops" && laptops.map(LaptopItem)}
+      {props.category == "smartwatches" && smartwatches.map(SmartwatchItem)}
     </div>
   )
 }
